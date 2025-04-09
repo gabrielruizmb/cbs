1234624f1d9ef078c33dc7d77a191baf22995411
Gabriel Ruiz Mussi Bersot <gabrielruizmb@gmail.com>
2025-04-09 11:58:16 -0300

    Added Client Entity

---
+++ src/main/java/com/example/features/Client.java
@@ -0,0 +1,27 @@
+package com.example.features;
+
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Client {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String phone;
+
+    private String adress;
+
+    private String secondaryContact;
+}
