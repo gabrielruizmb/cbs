866a8b60bf3f32abb15b820358a427ed751987e0
Gabriel Ruiz Mussi Bersot <gabrielruizmb@gmail.com>
2025-04-13 00:10:20 -0300

    Adicionado criar cliente

--- src/main/java/com/example/features/client/ClientCrud.java
+++ src/main/java/com/example/features/client/ClientCrud.java
@@ -9,5 +9,30 @@ public class ClientCrud {
         System.out.print("\n==============================\n");
         System.out.print("--- Sistema da Cris Ballon --- \n\n");
         System.out.print("~~~ Cadastrar cliente ~~~ \n\n");
+
+        System.out.print("Nome: ");
+        UserInterface.scanner.nextLine();   // Limpa o buffer do teclado.
+
+        // Enquanto o nome estiver em branco, avisa ao 
+        // usuário e pede para ele informar novamente.
+        while (!client.setName(UserInterface.scanner.nextLine())) {
+            System.out.print("O nome não pode ficar em branco!\nNome: ");
+        }
+        
+        System.out.print("Telefone: ");
+        // Enquanto o telefone estiver em branco, avisa ao 
+        // usuário e pede para ele informar novamente.
+        while (!client.setPhone(UserInterface.scanner.nextLine())) {
+            System.out.print("O telefone não pode ficar em branco!\n");
+            System.out.print("Telefone: ");
+        }
+
+        System.out.print("\nEndereço(opcional): ");
+        client.setAdress(UserInterface.scanner.nextLine());
+
+        System.out.print("\nContato secundário, ex: email, tel.(Opcional): ");
+        client.setSecondaryContact(UserInterface.scanner.nextLine());
+
+        
     }
 }
